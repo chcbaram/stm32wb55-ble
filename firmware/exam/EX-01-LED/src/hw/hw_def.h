d5afa1201b7f54a30785f82df8540eea125fa347
Hancheol Cho <chcbaram@gmail.com>
2024-01-18 15:36:35 +0900

    LED 예제 추가

---
+++ firmware/exam/EX-01-LED/src/hw/hw_def.h
@@ -0,0 +1,22 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+
+#include "bsp.h"
+
+
+#define _DEF_FIRMWATRE_VERSION    "V240118R1"
+#define _DEF_BOARD_NAME           "STM32WB55-BLE-FW"
+
+
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          3
+#define      HW_LED_CH_LED          _DEF_LED1
+#define      HW_LED_CH_LED2         _DEF_LED2
+#define      HW_LED_CH_BLE          _DEF_LED3
+
+
+#endif
